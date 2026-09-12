9496888bcf9b52399eb6c71f46a1f271b2f5e564
syedwahaj121 <wahajwania1@gmail.com>
2026-09-12 14:51:57 +0000

    class 4 chalrahye he

--- class3.py
+++ class3.py
@@ -1,21 +1,9 @@
-"""n1 = int(input("Enter the first number: "))
-n2 = int(input("Enter the second number: "))
-n3 = int(input("Enter the third number: "))
-n4 = int(input("Enter the fourth number: "))
-print(f"addition of {n1} and {n2} is: {n1 + n2} multi of  {n1} and {n2} is: {n1 * n2}")
-print(f"addition of {n3} and {n4} is: {n3 + n4} multi of  {n3} and {n4} is: {n3 * n4}")
-print(f"mod  of {n1} and {n2} is: {n1 % n2} div of  {n1} and {n2} is: {n1 // n2}")
-print(f"mod  of {n1} and {n2} is: {n1 % n2} div of  {n1} and {n2} is: {n1 / n2}")
-print(f"addition of {n3} and {n4} is: {n3 + n4} multi of  {n3} and {n4} is: {n3 * n4}")"""
-a = input("Enter the first name: ")
-b = input("Enter the second name: ")
-if a<b:
-    print(f"{a} is less than {b}")
-elif a==b:
-    print(f"{a} is equal to {b}")
+from colorama import Fore as color, Style
+a = "wahaj"
+b = "syed"
+user = input("Enter your name: ")
+password = input("Enter your password: ")
+if(user == a and password == b):
+    print(color.GREEN + "wellcome " + user)
 else:
-    print(f"{a} is greater than {b}")
-print(ord('A'))   # 65  → character to number
-print(ord('a'))   # 97
-print(chr(65))    # 'A' → number to character
-print(chr(97))    # 'a'
+    print(color.RED + "  bhag " + user)
